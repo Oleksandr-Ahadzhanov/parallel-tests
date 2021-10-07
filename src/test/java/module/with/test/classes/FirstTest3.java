7704baee8cf0b23191eb21c2b85282232974f08c
o.ahadzhanov <o.ahadzhanov@gmail.com>
2021-10-07 14:06:06 +0300

    first commit - structure

---
+++ src/test/java/module/with/test/classes/FirstTest3.java
@@ -0,0 +1,51 @@
+package module.with.test.classes;
+
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class FirstTest3 {
+
+    @Test
+    void oneTestMethod() throws Exception{
+        Thread.sleep(5000);
+    }
+    @Test
+    void twoTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void threeTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void fourTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void fiveTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void sixTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void sevenTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void eightTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void nineTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void tenTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+}
---
+++ src/test/java/module6/with/test/classes/FirstTest5.java
@@ -0,0 +1,47 @@
+package module6.with.test.classes;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTest5 {
+
+    @Test
+    void oneTestMethod() throws Exception{
+        Thread.sleep(5000);
+    }
+    @Test
+    void twoTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void threeTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void fourTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void fiveTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void sixTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void sevenTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void eightTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void nineTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+    @Test
+    void tenTestMethod() throws Exception {
+        Thread.sleep(5000);
+    }
+}
